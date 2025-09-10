686aacbc1b5ba462492f9036c4a9a656ee1e9532
RechalDevabarikena <rechaldevabarikena@gmail.com>
2025-09-10 08:05:10 +0530

    first commit

---
+++ File3.java
@@ -0,0 +1,11 @@
+class File
+{
+    void fun()
+    {
+        System.out.println("hello github");
+    }
+    public static void main(String args[])
+    {
+        fun();
+    }
+}
---
+++ file2.py
@@ -0,0 +1,1 @@
+print("pull deme file2")
